bccad88f55b2bc4fc3e22daf1fe6ee342844c4b7
Eric Perreault <eric@eric-f33.myfiosgateway.com>
2021-05-28 12:48:37 -0400

    first commit

---
+++ polls/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Definitions of data models.
+# Each model subclasses the django.db.models.Model class.
+# manage.py makemigrations polls >> update these models
+# table names generated as "app_model" (polls_question)
+
+class Question(models.Model):
+    # Each field is an instance of the Field class.
+    question_text = models.CharField(max_length=200)
+
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    # associates each Choice to a Question
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
