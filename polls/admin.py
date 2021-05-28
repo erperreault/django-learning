2c427cb967900e002f2ae3f8793a369224b00fb1
erperreault <erperreault@gmail.com>
2021-05-28 13:10:31 -0400

    check upstream

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question
+
+# This gives Question objects an admin interface
+admin.site.register(Question)
--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,8 @@
+import  datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 # Definitions of data models.
 # Each model subclasses the django.db.models.Model class.
@@ -11,9 +15,19 @@ class Question(models.Model):
 
     pub_date = models.DateTimeField('date published')
 
+    # str() methods make db more readable
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
     # associates each Choice to a Question
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
