1a74cb317145b9261bb117d15e28762e11bae1f4
erperreault <erperreault@gmail.com>
2021-05-28 13:44:28 -0400

    set up poll views

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     # path '' = root
     path('', views.index, name='index'),
+    # call detail function in views.py
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
