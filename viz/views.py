ddf6f6775294c612e356135083a6521b2a45172a
erperreault <erperreault@gmail.com>
2021-06-08 13:51:55 -0400

    get data from bgg, render to page

--- viz/views.py
+++ viz/views.py
@@ -6,11 +6,17 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+import xml.etree.ElementTree as et
+import urllib.request as rq
+from viz.utils import fetch_bgg_data, parse_xml
+
+games = {"Agricola":31260, "Avalon":128882, "Azul":230802, "Keyflower":122515, "Kingdom Builder":107529}
+
 def index(request):
-    df = sns.load_dataset('penguins')
+    df = parse_xml(fetch_bgg_data(games.values()))
 
-    sns.pairplot(df, hue='species')
+    print(df)
 
-    plt.savefig('viz/static/viz/test.png')
-
+    sns.lineplot(data=df, x="yearpublished", y="maxplayers")
+    plt.savefig(f'viz/static/viz/test.png') # save file to be referenced in index.html
     return render(request, 'viz/index.html') 
