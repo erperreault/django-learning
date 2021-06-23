95666a91f8ff15c3206cc83fbcc7894eeda22597
Eric Perreault <erperreault@gmail.com>
2021-06-23 19:02:52 -0400

    now renders! next add filters for outliers / select ranges

--- viz/forms.py
+++ viz/forms.py
@@ -2,7 +2,13 @@ from django import forms
 
 data_fields = [
     ('yearpublished', 'Year Published'),
+    ('minplayers', 'Min Players'),
     ('maxplayers', 'Max Players'),
+    ('playingtime', 'Playing Time'),
+    ('minplaytime', 'Min Play Time'),
+    ('maxplaytime', 'Max Play Time'),
+    ('age', 'Age'),
+    ('boardgamepublisher', 'Publisher'),
 ]
 
 class BGGForm(forms.Form):
--- viz/views.py
+++ viz/views.py
@@ -1,17 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
+from .BGGClient import BGGClient
+from .Grapher import Grapher
 from .utils import test_render
 from .forms import BGGForm
+from .test_data import supported_fields
 
 def form(request):
     if request.method == 'POST':
         form = BGGForm(request.POST)
 
         if form.is_valid():
-            print(form.cleaned_data)
-            test_render(form.cleaned_data)
-            return HttpResponseRedirect('/chart/')
+            client = BGGClient(form.cleaned_data['username'])
+            df = client.yield_dataframe(supported_fields)
+            grapher = Grapher(df)
+
+            x_axis = form.cleaned_data['x_axis']
+            y_axis = form.cleaned_data['y_axis']
+            grapher.scatter(x_axis, y_axis)
+            return HttpResponseRedirect('chart')
 
     else:
         form = BGGForm()
