15b52c79938b123d186e3b6c024e46133a24085f
erperreault <erperreault@gmail.com>
2021-07-07 09:26:03 -0400

    handle username not found gracefully

--- viz/Grapher.py
+++ viz/Grapher.py
@@ -1,4 +1,3 @@
-
 import seaborn as sns
 import matplotlib.pyplot as plt
 from .data import test_chart_path
@@ -12,7 +11,6 @@ class Grapher:
         self.dataframe = df
 
     def scatter(self, x_axis, y_axis):
-        print(self.dataframe)
         sns.scatterplot(data=self.dataframe, x=x_axis, y=y_axis)
         plt.savefig(test_chart_path)
         plt.clf()
